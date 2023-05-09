2f850a1c0e9201513b6fdc6a03814cff9d7eecd2
julio-py <63744748+julio-py@users.noreply.github.com>
2023-05-09 16:21:53 +0200

    fixes

--- src/train.py
+++ src/train.py
@@ -17,7 +17,7 @@ def train():
     # Create benchmarks by training on multiple datasets sequentially
     if args.bulk_train:
         cl_model_name = args.model.path.replace("/","_")
-        paths = glob(args.data_path + f'\*{cl_model_name}*')
+        paths = glob(os.path.join(args.paths.data_path, f"*{cl_model_name}*"))
         save_paths = [os.path.join(args.paths.save_path, f"{p[mode_idx:]}_{cl_model_name}" )for p in paths if (mode_idx:=p.rfind("q"))]
         mode_to_modeltype = {"q_p_a": "T5Concat", "q_a": "T5ForConditionalGeneration", "q_pa": "T5ForConditionalGeneration", "qp_a": "T5ForConditionalGeneration"}
         model_types = [mode_to_modeltype[p[mode_idx:]] for p in paths if (mode_idx:=p.rfind("q"))]
@@ -26,7 +26,7 @@ def train():
         save_paths = [args.paths.save_path]
         model_types = [args.model.model_type]
     
-    for save_path, path, model_type in zip(paths, save_paths,model_types):
+    for path,save_path, model_type in zip(paths, save_paths,model_types):
         dataset = load_from_disk(path)
         os.makedirs(save_path, exist_ok=True)
         if args.optim.total_steps is None: args.optim.total_steps = len(dataset["train"]) / args.train.grad_acc_steps * args.train.epochs
@@ -70,7 +70,7 @@ def train():
         )
 
         trainer.train()
-        trainer.save_model(args.paths.save_path)
+        trainer.save_model(save_path)
 
 
 
